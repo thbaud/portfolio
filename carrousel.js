83198bc7ba6bd99591c599edf333ec426c6bb506
thbaud <baudrillart.pro@gmail.com>
2023-06-23 18:40:58 +0200

    carrousel wip

--- carrousel.js
+++ carrousel.js
@@ -1,8 +1,32 @@
+let bt1 = document.getElementById('btn1');
+let info1 = document.getElementById('info1');
+
+function hide () {
+    if(info1.className === ".infos.active") {
+        bt1.style.display = "none";
+    } else {
+        bt1.style.display = "block";
+    }
+};
+
 btn2.addEventListener("click", () => {
     
-    let infosActive = document.querySelectorAll(".infos.active");
-        infosActive.classList.remove("active");
-        infosActive.nextElementSibling.add("active");
+    let infosActive = document.querySelector(".infos.active");
+            infosActive.classList.remove("active");
+            infosActive.nextElementSibling.classList.add("active");
+        
     
 });
+
+btn1.addEventListener("click", () => {
+    
+    let infosActive = document.querySelector(".infos.active");
+            infosActive.classList.remove("active");
+            infosActive.previousElementSibling.classList.add("active");
+        
+    
+});
+
+
+
 
