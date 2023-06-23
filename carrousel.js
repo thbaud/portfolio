839538217a5fa90a534893da3c8d6df19ab0b437
thbaud <baudrillart.pro@gmail.com>
2023-06-23 21:51:54 +0200

    carrousel working progress btn1 et 2 ok

--- carrousel.js
+++ carrousel.js
@@ -1,31 +1,57 @@
 let bt1 = document.getElementById('btn1');
+let bt2 = document.getElementById('btn2');
 let info1 = document.getElementById('info1');
+let info2 = document.getElementById('info2');
+let info3 = document.getElementById('info3');
+  
+    
 
-function hide () {
-    if(info1.className === ".infos.active") {
-        bt1.style.display = "none";
-    } else {
-        bt1.style.display = "block";
-    }
-};
 
 btn2.addEventListener("click", () => {
-    
     let infosActive = document.querySelector(".infos.active");
-            infosActive.classList.remove("active");
-            infosActive.nextElementSibling.classList.add("active");
+        infosActive.classList.remove("active");
+        infosActive.nextElementSibling.classList.add("active"); 
+    
+    if (info3.className === "infos active") 
+        bt2.style.display = "none";
+    else if (info2.className === "infos active") 
+        bt2.style.display = "block";
+    else if (info1.className === "infos active") 
+        bt2.style.display = "block";
+
+        if (info3.className === "infos active") 
+        bt1.style.display = "block";
+    else if (info2.className === "infos active") 
+        bt1.style.display = "block";
+    else if (info1.className === "infos active") 
+        bt1.style.display = "none";
         
+
     
 });
 
+
 btn1.addEventListener("click", () => {
-    
     let infosActive = document.querySelector(".infos.active");
-            infosActive.classList.remove("active");
-            infosActive.previousElementSibling.classList.add("active");
+        infosActive.classList.remove("active");
+        infosActive.previousElementSibling.classList.add("active");
         
-    
+    if (info3.className === "infos active")
+        bt2.style.display = "none";
+    else if (info2.className === "infos active")
+        bt2.style.display = "block";
+    else if (info1.className === "infos active")
+        bt2.style.display = "block";
+
+        if (info3.className === "infos active")
+        bt1.style.display = "block";
+    else if (info2.className === "infos active")
+        bt1.style.display = "block";
+    else if (info1.className === "infos active")
+        bt1.style.display = "none";
+        
 });
+
 
 
 
