c3970b0f1b80b1c44f135d1e48ed0789fc87f6eb
thbaud <baudrillart.pro@gmail.com>
2023-07-03 22:07:08 +0200

    carrousel infini + fix animation load

--- carrousel.js
+++ carrousel.js
@@ -4,37 +4,6 @@ let info1 = document.getElementById('info1');
 let info2 = document.getElementById('info2');
 let info3 = document.getElementById('info3');
 
-    function flecheHidden() {
-            
-        if (info3.className === "infos info-active")
-            bt2.style.display = "none";
-        else if (info2.className === "infos info-active")
-            bt2.style.display = "block";
-        else if (info1.className === "infos info-active")
-            bt2.style.display = "block";
-            
-                
-        if (info3.className === "infos info-active")
-            bt1.style.display = "block";
-        else if (info2.className === "infos info-active")
-            bt1.style.display = "block";
-        else if (info1.className === "infos info-active")
-            bt1.style.display = "none";
-}
-
-    function flecheMargin() {
-
-        if (info3.className === "infos info-active"){
-            containerInfos.style.marginRight = "70px";
-            containerInfos.style.marginLeft = "0";
-        } else if (info2.className === "infos info-active"){
-            containerInfos.style.margin = "0";
-        } else if (info1.className === "infos info-active"){
-            containerInfos.style.marginLeft = "70px";
-            containerInfos.style.marginRight = "0";
-    }         
-}
-
     function pointActive (){
 
         if (info3.className === "infos info-active") {
@@ -64,21 +33,34 @@ let info3 = document.getElementById('info3');
 
     btn2.addEventListener("click", () => {
         let infosActive = document.querySelector(".infos.info-active");
-            infosActive.classList.remove("info-active");
-            infosActive.nextElementSibling.classList.add("info-active");
             
-        flecheHidden();
-        flecheMargin();
+            if (info3.className === "infos info-active") {
+                infosActive.classList.remove("info-active");
+                info1.classList.add("info-active");
+            } else if (info2.className === "infos info-active") {
+                infosActive.classList.remove("info-active");
+                infosActive.nextElementSibling.classList.add("info-active");
+            } else if (info1.className === "infos info-active") {
+                infosActive.classList.remove("info-active");
+                infosActive.nextElementSibling.classList.add("info-active");
+        }
+    
         pointActive ();           
 });
 
     btn1.addEventListener("click", () => {
         let infosActive = document.querySelector(".infos.info-active");
-            infosActive.classList.remove("info-active");
-            infosActive.previousElementSibling.classList.add("info-active"); 
-
-        flecheHidden();
-        flecheMargin();
+       
+            if (info3.className === "infos info-active") {
+                infosActive.classList.remove("info-active");
+                infosActive.previousElementSibling.classList.add("info-active"); 
+            } else if (info2.className === "infos info-active") {
+                infosActive.classList.remove("info-active");
+                infosActive.previousElementSibling.classList.add("info-active"); 
+            } else if (info1.className === "infos info-active") {
+                infosActive.classList.remove("info-active");
+                info3.classList.add("info-active");
+        }
         pointActive ();
 });
 
@@ -87,8 +69,6 @@ let info3 = document.getElementById('info3');
             infosActive.classList.remove("info-active");
             info1.classList.add("info-active");
 
-        flecheHidden();
-        flecheMargin();
         pointActive ();
 });        
 
@@ -97,8 +77,6 @@ let info3 = document.getElementById('info3');
             infosActive.classList.remove("info-active");
             info3.classList.add("info-active");
                
-        flecheHidden();
-        flecheMargin();
         pointActive ();
 });
 
@@ -107,7 +85,5 @@ let info3 = document.getElementById('info3');
             infosActive.classList.remove("info-active");
             info2.classList.add("info-active");
             
-        flecheHidden();
-        flecheMargin();
         pointActive ();
 });
